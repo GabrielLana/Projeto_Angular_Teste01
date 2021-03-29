21ab6d6a91850c6b57e8ae7d5519d06f6fc29477
Gabriel Lana <gabriellanapro@gmail.com>
2021-03-29 14:14:43 -0300

    Teste routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '',  component: AboutComponent},
-  { path: 'about',  component: TodosComponent}
+  { path: '',  component: TodosComponent},
+  { path: 'about',  component: AboutComponent}
 ];
 
 @NgModule({
